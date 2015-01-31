44b7b6dc36032549134ab1da12a98e7ae3e28976
Dennis Oberhoff <dennisoberhoff@gmail.com>
2015-01-31 09:38:16 +0100

    Improvement for no WiFi enabled fetching

--- VBBNow/VBBTodayViewController.m
+++ VBBNow/VBBTodayViewController.m
@@ -13,15 +13,12 @@
 #import "VBBListRowViewController.h"
 #import "VBBNetworkManager.h"
 
-typedef void (^didUpdateLocationBlock)(NSArray *locations);
+typedef void (^didUpdateLocationBlock)(CLLocation *location);
 typedef void (^didChangeAuthorizationStatus)(CLAuthorizationStatus status);
 
 @interface VBBTodayViewController () <NCWidgetProviding, NCWidgetListViewDelegate, CLLocationManagerDelegate>
 
-@property (strong) IBOutlet NCWidgetListViewController *listViewController;
-
-@property (nonatomic, readwrite, strong) NSArray *stations;
-
+@property (nonatomic, readwrite, strong) IBOutlet NCWidgetListViewController *listViewController;
 @property (nonatomic, readwrite, strong) CLLocationManager *locationManager;
 @property (nonatomic, readwrite, copy) didUpdateLocationBlock didUpdateLocationBlock;
 @property (nonatomic, readwrite, copy) didChangeAuthorizationStatus didChangeAuthorizationStatus;
@@ -37,15 +34,13 @@ typedef void (^didChangeAuthorizationStatus)(CLAuthorizationStatus status);
     [NSURLCache setSharedURLCache:nil];
     self.locationManager = [CLLocationManager new];
     self.locationManager.delegate = self;
-    [self reloadDataForLocation:[self storedLocation]];
 
 }
 
 -(void)reloadDataForLocation:(CLLocation*)location {
     
     if (!location) return;
-    self.stations = [[VBBStation class] sortByDistance:location andLimit:5];
-    self.listViewController.contents = self.stations;
+    self.listViewController.contents = [[VBBStation class] sortByDistance:location andLimit:5];
     
 }
 
@@ -54,24 +49,25 @@ typedef void (^didChangeAuthorizationStatus)(CLAuthorizationStatus status);
 -(void)fetchNearby: (void (^)(NCUpdateResult result))completionHandler {
  
     __block typeof(self) blockSelf = self;
-    if ([[CLLocationManager class] authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
-        [self setDidChangeAuthorizationStatus:^(CLAuthorizationStatus status){
-            if (status != kCLAuthorizationStatusAuthorized) return;
-            [blockSelf fetchNearby:completionHandler];
-        }];
-    }
-    
-    [self setDidUpdateLocationBlock:^(NSArray *locations){
-        CLLocation *location = locations.firstObject;
-        [blockSelf storeLocation:location];
-        if (!location) return;
+    void (^responseBlock)(CLLocation *location) = ^void(CLLocation *location) {
+        if (location) [blockSelf storeLocation:location];
+        else location = [blockSelf storedLocation];
+        if (!location) {
+            completionHandler(NCUpdateResultFailed);
+            return;
+        }
         [[VBBNetworkManager manager] fetchNearedStations:location andCompletionHandler:^(NSArray *stations) {
-            blockSelf.stations = stations;
             [blockSelf reloadDataForLocation:location];
             completionHandler(NCUpdateResultNewData);
         }];
-    }];
+        
+    };
     
+    if ([[CLLocationManager class] authorizationStatus] != kCLAuthorizationStatusAuthorized) {
+        responseBlock([self storedLocation]);
+    }
+    
+    [self setDidUpdateLocationBlock:responseBlock];
     [self.locationManager startUpdatingLocation];
     
 }
@@ -125,7 +121,7 @@ typedef void (^didChangeAuthorizationStatus)(CLAuthorizationStatus status);
 
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
     if (self.didUpdateLocationBlock) {
-        self.didUpdateLocationBlock(locations);
+        self.didUpdateLocationBlock(locations.firstObject);
         self.didUpdateLocationBlock = nil;
     }
 }
